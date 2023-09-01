3cf95de853cc4acc36ceceffa241853fe8420aeb
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2023-09-01 14:57:44 -0600

    Initial commit

---
+++ src/test/java/dev/marvin/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.marvin.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
